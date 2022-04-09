dd42bc9c67de1b9bd294dd9d90e20e9e2b45fe95
minu <anomie7777@gmail.com>
2022-04-09 16:51:25 +0900

    feat: CartRepository에서 map을 Cart로 변환하는 중복 로직 제거

--- r2dbc-example/src/main/kotlin/com/spring/kotlin/reactive/r2dbc/repository/CartRepository.kt
+++ r2dbc-example/src/main/kotlin/com/spring/kotlin/reactive/r2dbc/repository/CartRepository.kt
@@ -8,7 +8,6 @@ import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import org.springframework.r2dbc.core.DatabaseClient
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
-import java.time.Duration
 import java.util.stream.Collectors
 
 @Repository
@@ -24,6 +23,34 @@ interface CartCustomRepository {
 
 @Repository
 class CartCustomRepositoryImpl(val dataBaseClient: DatabaseClient) : CartCustomRepository {
+    private val cartMapper: (t: MutableList<MutableMap<String, Any>>) -> Cart
+        get() {
+            val cartMapper: (t: MutableList<MutableMap<String, Any>>) -> Cart = { list ->
+                val cartId = list[0]["cart_id"] as Long
+                val cartItems = list.stream().map {
+                    val id = it["id"] as Long
+                    val quantity = it["quantity"] as Int
+                    val cartId = it["cart_id"] as Long
+                    val itemId = it["item_id"] as Long
+                    val name = it["item_name"] as String
+                    val price = it["item_price"] as Double
+                    CartItem(
+                        id = id,
+                        quantity = quantity,
+                        cartId = cartId,
+                        itemId = itemId,
+                        Item(
+                            id = itemId,
+                            name = name,
+                            price = price
+                        )
+                    )
+                }.collect(Collectors.toList())
+                Cart(id = cartId, cartItems = cartItems)
+            }
+            return cartMapper
+        }
+
     override fun getAll(): Flux<Cart> {
         return dataBaseClient.sql(
             """
@@ -34,29 +61,7 @@ class CartCustomRepositoryImpl(val dataBaseClient: DatabaseClient) : CartCustomR
         ).fetch().all()
             .bufferUntilChanged {
                 it["cart_id"]
-            }.map { list ->
-                val cartId = list[0]["cart_id"] as Long
-                val cartItems = list.stream().map {
-                    val id = it["id"] as Long
-                    val quantity = it["quantity"] as Int
-                    val cartId = it["cart_id"] as Long
-                    val itemId = it["item_id"] as Long
-                    val name = it["item_name"] as String
-                    val price = it["item_price"] as Double
-                    CartItem(
-                        id = id,
-                        quantity = quantity,
-                        cartId = cartId,
-                        itemId = itemId,
-                        Item(
-                            id = itemId,
-                            name = name,
-                            price = price
-                        )
-                    )
-                }.collect(Collectors.toList())
-                Cart(id = cartId, cartItems = cartItems)
-            }
+            }.map(cartMapper)
 //            .delayElements(Duration.ofSeconds(5))
     }
 
@@ -66,33 +71,17 @@ class CartCustomRepositoryImpl(val dataBaseClient: DatabaseClient) : CartCustomR
      * Cart 조회시 모든 연관관계를 즉시 불러옴
      */
     override fun getById(cartId: Long): Flux<Cart> {
-        return dataBaseClient.sql("SELECT cart_item.*, item.name as item_name, item.price as item_price FROM cart_item INNER JOIN item ON cart_item.item_id = item.id WHERE cart_item.cart_id = :cart_id")
+        return dataBaseClient.sql(
+            """
+                SELECT cart_item.*, item.name as item_name, item.price as item_price FROM cart_item
+                INNER JOIN item ON cart_item.item_id = item.id
+                WHERE cart_item.cart_id = :cart_id
+            """.trimMargin()
+        )
             .bind("cart_id", cartId)
             .fetch().all()
             .bufferUntilChanged {
                 it["cart_id"]
-            }.map { list ->
-                val cartId = list[0]["cart_id"] as Long
-                val cartItems = list.stream().map {
-                    val id = it["id"] as Long
-                    val quantity = it["quantity"] as Int
-                    val cartId = it["cart_id"] as Long
-                    val itemId = it["item_id"] as Long
-                    val name = it["item_name"] as String
-                    val price = it["item_price"] as Double
-                    CartItem(
-                        id = id,
-                        quantity = quantity,
-                        cartId = cartId,
-                        itemId = itemId,
-                        Item(
-                            id = itemId,
-                            name = name,
-                            price = price
-                        )
-                    )
-                }.collect(Collectors.toList())
-                Cart(id = cartId, cartItems = cartItems)
-            }
+            }.map(cartMapper)
     }
 }
