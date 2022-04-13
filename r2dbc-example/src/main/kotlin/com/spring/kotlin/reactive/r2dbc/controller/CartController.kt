6801e9d568191bca70837e4f9cbbff508a4ef2c6
minu <anomie7777@gmail.com>
2022-04-14 00:36:43 +0900

    feat: cart에 item을 추가하는 api 구현

--- r2dbc-example/src/main/kotlin/com/spring/kotlin/reactive/r2dbc/controller/CartController.kt
+++ r2dbc-example/src/main/kotlin/com/spring/kotlin/reactive/r2dbc/controller/CartController.kt
@@ -1,15 +1,20 @@
 package com.spring.kotlin.reactive.r2dbc.controller
 
 import com.spring.kotlin.reactive.r2dbc.entity.Cart
+import com.spring.kotlin.reactive.r2dbc.entity.CartItem
 import com.spring.kotlin.reactive.r2dbc.repository.CartRepository
+import com.spring.kotlin.reactive.r2dbc.repository.ItemRepository
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import java.lang.RuntimeException
 
 @RestController
-class CartController(val cartRepository: CartRepository) {
+class CartController(val cartRepository: CartRepository, val itemRepository: ItemRepository) {
 
     @GetMapping("v1/carts")
     fun getCarts(): Flux<Cart> {
@@ -24,5 +29,14 @@ class CartController(val cartRepository: CartRepository) {
     @GetMapping("v1/carts/{id}")
     fun getCartsById(@PathVariable("id") id: Long): Flux<Cart> {
         return cartRepository.getById(id)
+    }
+
+    @PutMapping("v1/carts/{id}/cartItems/{itemId}")
+    fun addItem(@PathVariable("id") cartId: Long, @PathVariable("itemId") itemId: Long): Flux<CartItem> {
+        return itemRepository.findById(itemId)
+            .switchIfEmpty(Mono.error(RuntimeException("item not founded $itemId")))
+            .flatMapMany {
+                cartRepository.addItemToCart(cartId, itemId)
+            }
     }
 }
